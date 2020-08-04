f944d5574cfa1ee7f7bbcd5d2f39f445c8536c1b
xiaolin <462441355@qq.com>
2020-08-04 21:36:50 +0800

    * ceshi

--- qk-base/src/main.js
+++ qk-base/src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
+
 Vue.config.productionTip = false
 
 new Vue({
@@ -10,3 +15,4 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
+  
