4ef8775856e474e4203a577cab5815ae74b6cb87
xiaolin <462441355@qq.com>
2020-08-04 21:46:24 +0800

    * 增加qiankun基座

--- qk-base/src/App.vue
+++ qk-base/src/App.vue
@@ -1,16 +1,15 @@
 <template>
   <div id="app">
     <div id="nav">
-      <el-menu :router="true">
-        <el-menu-item to="/">Home</el-menu-item>
-        <el-menu-item to="/vue">vue应用</el-menu-item>
-        <el-menu-item to="/react">react应用</el-menu-item>
+      <el-menu :router="true" mode="horizontal">
+        <el-menu-item index="/">Home</el-menu-item>
+        <el-menu-item index="/vue">vue应用</el-menu-item>
+        <el-menu-item index="/react">react应用</el-menu-item>
       </el-menu>
       <router-view></router-view>
       <div id="vue"></div>
       <div id="react"></div>
     </div>
-    <router-view/>
   </div>
 </template>
 
--- qk-base/src/main.js
+++ qk-base/src/main.js
@@ -6,6 +6,29 @@ import store from './store'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
+import { registerMicroApps, start } from 'qiankun'
+
+const apps = [
+  {
+    name: 'vueApp',
+    entry: '//localhost:10000', // 默认会加载这个html 解析里面的js 动态执行 (子应用必须支持跨域), fetch
+    container: "#vue", // 容器名称
+    activeRule: '/vue' // 激活路径
+
+  },
+  {
+    name: 'reactApp',
+    entry: '//localhost:20000', // 默认会加载这个html 解析里面的js 动态执行 (子应用必须支持跨域), fetch
+    container: "#react",
+    activeRule: '/react'
+
+  }
+]
+registerMicroApps(apps)
+start({
+  prefetch: false // 取消预加载
+})
+
 Vue.use(ElementUI);
 
 Vue.config.productionTip = false
