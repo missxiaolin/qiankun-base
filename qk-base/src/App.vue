a0934c0ff3bb2c9eaa24d489514d2c1ab6f5f177
xiaolin <462441355@qq.com>
2020-08-04 22:51:09 +0800

    * 启动完成

--- qk-base/src/App.vue
+++ qk-base/src/App.vue
@@ -1,18 +1,18 @@
 <template>
-  <div id="app">
+  <div>
     <div id="nav">
       <el-menu :router="true" mode="horizontal">
         <el-menu-item index="/">Home</el-menu-item>
         <el-menu-item index="/vue">vue应用</el-menu-item>
         <el-menu-item index="/react">react应用</el-menu-item>
       </el-menu>
-      <router-view></router-view>
-      <div id="vue"></div>
-      <div id="react"></div>
     </div>
+
+    <router-view></router-view>
+    <div id="vue"></div>
+    <div id="react"></div>
   </div>
 </template>
 
 <style>
-
 </style>
--- vue-child/vue.config.js
+++ vue-child/vue.config.js
@@ -1,15 +1,15 @@
 module.exports = {
     lintOnSave: false,
-    devServer: {
-        port: 10000,
-        headers: {
-            'Access-Control-Allow-Origin': '*'
+    devServer:{
+        port:10000,
+        headers:{
+            'Access-Control-Allow-Origin':'*'
         }
     },
-    configureWebpack: {
-        output: {
-            library: 'vueApp',
-            libraryTarget: 'umd',
+    configureWebpack:{
+        output:{
+            library:'vueApp',
+            libraryTarget:'umd'
         }
     }
 }
