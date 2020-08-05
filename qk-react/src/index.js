212f3bbf8fd18d7009cc5a8e600e38ac3b73c853
xiaolin <462441355@qq.com>
2020-08-05 09:58:40 +0800

    * react 测试

--- qk-react/src/index.js
+++ qk-react/src/index.js
@@ -2,16 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function render(){
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+if(!window.__POWERED_BY_QIANKUN__){
+  render();
+}
+export async function bootstrap(){
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+}
+export async function mount() {
+  render()
+}
+export async function unmount(){
+  ReactDOM.unmountComponentAtNode( document.getElementById('root'));
+}
